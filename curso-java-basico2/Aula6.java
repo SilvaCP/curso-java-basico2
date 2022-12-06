2b04ce3ef80c766ccc83c501867bf0679eec46c0
Carlos <pereiracarlos1047@gmail.com>
2022-12-05 23:02:17 -0300

    Commit Init

---
+++ curso-java-basico2/Aula6.java
@@ -0,0 +1,11 @@
+/**
+ * @author carlos
+ * Aula 06 - Tipos Primitivos e Manipulação de dados.
+ * */
+public class Aula6 {
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+
+	}
+
+}
